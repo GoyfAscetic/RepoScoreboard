b9209255420e424ecbde4eb9cc64ebfa45c7b075
Adam Eidelsafy <aheidels@gmail.com>
2018-04-02 14:18:10 -0400

    Removed promise code

--- repo_score.js
+++ repo_score.js
@@ -16,30 +16,26 @@ populatefiles("googleplay-api", "master:");
 
 //Find all the files in a repo for a given path
 function populatefiles(repo_name, path){
-    var deferred = Promise.defer();
-    
     //Query the GitHub API for all files in repo <repo_name>
     //found at <path>
     github.query(`
-        {
-            viewer {
-                repository(name: "` + repo_name +`") {
-                    object(expression: "` + path +`") {
-                        ... on Tree{
-                            entries{
-                                name
-                                type
-                                mode
-                            }
+    {
+        viewer {
+            repository(name: "` + repo_name +`") {
+                object(expression: "` + path +`") {
+                    ... on Tree{
+                        entries{
+                            name
+                            type
+                            mode
                         }
                     }
                 }
             }
-        }`, null, 
-    (res, err) => {
-        if(err){
-            deferred.reject(err);
         }
+    }`, null, 
+    (res, err) => {
+        
 
         //The array of files returned by Github
         var entries  = res.data.viewer.repository.object.entries
@@ -55,16 +51,13 @@ function populatefiles(repo_name, path){
                 console.log(newpath + " is a tree!");
 
                 //call the function with the new path
-                populatefiles(repo_name, newpath)
-                .then(function() {
-                    deferred.resolve();
-                });;
+                populatefiles(repo_name, newpath);
             }
         }
 
-        return deferred.promise;
         //log all entries found at <path>
         console.log(path + " contents");
         //console.log(JSON.stringify(res, null, 2))
     })
+    
 }
